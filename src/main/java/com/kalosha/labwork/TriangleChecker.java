6fa30bead4029a2d6bb6a7a60ed91598e7230832
Kavazar <zaharkalosha@gmail.com>
2024-11-24 20:58:54 +0200

    Update code to be more universal and a part into README.md

--- src/main/java/com/kalosha/labwork/TriangleChecker.java
+++ src/main/java/com/kalosha/labwork/TriangleChecker.java
@@ -25,9 +25,38 @@ public class TriangleChecker {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter three non-negative integers:");
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        int c = scanner.nextInt();
+        int a = Integer.MIN_VALUE;
+        int b = Integer.MIN_VALUE;
+        int c = Integer.MIN_VALUE;
+        while (a < 0 || b < 0 || c < 0) {
+            try {
+                if(a < 0) {
+                    a = scanner.nextInt();
+                }
+                if (a < 0) {
+                    System.out.println("Please enter three non-negative integers:");
+                    continue;
+                }
+                if(b < 0) {
+                    b = scanner.nextInt();
+                }
+                if (b < 0) {
+                    System.out.println("Please enter three non-negative integers:");
+                    continue;
+                }
+                if(c < 0) {
+                    c = scanner.nextInt();
+                }
+                if (c < 0) {
+                    System.out.println("Please enter three non-negative integers:");
+                    continue;
+                }
+                break;
+            } catch (Exception e) {
+                System.out.println("Please enter three non-negative integers:");
+                scanner.nextLine();
+            }
+        }
 
         if (isTriangle(a, b, c)) {
             System.out.println("Triangle type: " + triangleType(a, b, c));
